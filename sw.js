2748e79ec1ad57a23c299a8bb681f6a2e6e33c09
yonfri-one <yonfri.one@gmail.com>
2026-07-31 20:18:52 -0300

    New

---
+++ sw.js
@@ -0,0 +1,3 @@
+self.addEventListener('install', e => e.waitUntil(skipWaiting()));
+self.addEventListener('activate', e => e.waitUntil(clients.claim()));
+self.addEventListener('fetch', e => e.respondWith(fetch(e.request)));
